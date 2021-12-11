fc9497aaf9c9bc64dc058507758217b6f3b189d2
Jackb-03 <jackbialek2003@gmail.com>
2021-12-11 10:37:48 +1030

    New routes
    Added parent information route and page
    Added FAQ route and page
    
    commented out mascot test

--- index.js
+++ index.js
@@ -9,26 +9,23 @@ app.set('view engine', 'ejs');
 
 // index page
 app.get('/', function(req, res) {
-  var mascots = [
-    { name: 'Sammy', organization: "DigitalOcean", birth_year: 2012},
+  //var mascots = [
+    //{ name: 'Sammy', organization: "DigitalOcean", birth_year: 2012},
 
-  ];
+ // ];
   var tagline = "No programming concept is complete without a cute animal mascot.";
 
   res.render('pages/index', {
-    mascots: mascots,
-    tagline: tagline
+    //mascots: mascots,
+    //tagline: tagline
   });
 });
 
-// about page
-app.get('/about', function(req, res) {
-  res.render('pages/about');
-});
 
-// pricing page
-app.get('/pricing', function(req, res) {
-  res.render('pages/pricing');
+
+// about-us page
+app.get('/about-us', function(req, res) {
+  res.render('pages/about');
 });
 
 // services page
@@ -36,10 +33,28 @@ app.get('/services', function(req, res) {
   res.render('pages/services');
 });
 
+// parent information
+app.get('/parent-info', function(req, res) {
+  res.render('pages/parent');
+});
+
+// FAQ page
+app.get('/FAQ', function(req, res) {
+  res.render('pages/FAQ');
+});
+
+// pricing page
+app.get('/pricing', function(req, res) {
+  res.render('pages/pricing');
+});
+
 // contact page
 app.get('/contact', function(req, res) {
   res.render('pages/contact');
 });
+
+
+
 
 app.listen(8080);
 console.log('Server is listening on port http://192.168.0.101:8080');
