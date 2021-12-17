e4d303e55b3d2379f8921067c91b187142322701
Jackb-03 <jackbialek2003@gmail.com>
2021-12-17 12:02:08 +1030

    Name change
    index to app

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
 var express = require('express');
 var app = express();
 
-app.use( express.static( "public" ) );
+app.use(express.static("public"));
 
 // set the view engine to ejs
 app.set('view engine', 'ejs');
 
 
 // index page
-app.get('/', function(req, res) {
+app.get('/', function (req, res) {
   //var mascots = [
-    //{ name: 'Sammy', organization: "DigitalOcean", birth_year: 2012},
+  //{ name: 'Sammy', organization: "DigitalOcean", birth_year: 2012},
 
- // ];
+  // ];
   var tagline = "No programming concept is complete without a cute animal mascot.";
 
   res.render('pages/index', {
@@ -24,32 +24,32 @@ app.get('/', function(req, res) {
 
 
 // about-us page
-app.get('/about-us', function(req, res) {
+app.get('/about-us', function (req, res) {
   res.render('pages/about');
 });
 
 // services page
-app.get('/services', function(req, res) {
+app.get('/services', function (req, res) {
   res.render('pages/services');
 });
 
 // parent information
-app.get('/parent-info', function(req, res) {
+app.get('/parent-info', function (req, res) {
   res.render('pages/parent');
 });
 
 // FAQ page
-app.get('/FAQ', function(req, res) {
+app.get('/FAQ', function (req, res) {
   res.render('pages/FAQ');
 });
 
 // pricing page
-app.get('/pricing', function(req, res) {
+app.get('/pricing', function (req, res) {
   res.render('pages/pricing');
 });
 
 // contact page
-app.get('/contact', function(req, res) {
+app.get('/contact', function (req, res) {
   res.render('pages/contact');
 });
 
